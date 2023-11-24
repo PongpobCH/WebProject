9827f40f71cfe33ef071f6df855a9a51f9529309
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-11-24 18:34:49 +0700

    EDIT: Profile

--- side_nav.js
+++ side_nav.js
@@ -1,11 +1,32 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
   }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+  /*const upload = multer({ storage: storage, fileFilter: imageFilter });
+//ทำให้สมบูรณ์
+app.post('/profilepic', upload.single('avatar'), (req, res) => {
+    if (!req.file) {
+        return res.redirect('resume.html?error=1');
+    }
+
+    const email = req.cookies.email;
+
+    updateImg(email, req.file.filename);
+
+    res.cookie('img', req.file.filename);
+
+    return res.redirect('resume.html');
+})
+
+const updateImg = async (email, filen) => {
+
+    let sql = UPDATE Userdatabase SET img = '${filen}' WHERE email = '${email}'
+    let result = await queryDB(sql);
+
+};*/
