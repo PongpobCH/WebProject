cce0847328beb018f6963c1ad261572cec8bfde1
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-11-27 23:23:53 +0700

    EDIT: CSS

--- public/js/feed.js
+++ public/js/feed.js
@@ -117,6 +117,7 @@ function checkCookie() {
 	}
   }
 
+  
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
